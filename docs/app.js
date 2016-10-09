079a2cd0ebf7d8f4a9469de8be9dd2794eefe4e6
tonyrft <satouakatsuki@gmail.com>
2016-10-09 18:24:43 -0400

    add exporter && finder

---
+++ docs/app.js
@@ -0,0 +1,10 @@
+new Clipboard('.BOOM-btn');
+var vue_app = new Vue({
+    el: '#app',
+    data: {
+        message: '',
+        answers: answerKey
+    },
+    methods: {
+    }
+})
--- export.py
+++ export.py
@@ -4,7 +4,7 @@ import re
 
 
 file = []
-fd = {}
+fd = []
 cwd = os.getcwd()
 for (dirpath, dirnames, filenames) in os.walk(cwd):
     for files in filenames:
@@ -12,5 +12,8 @@ for (dirpath, dirnames, filenames) in os.walk(cwd):
             file.append([os.path.join(dirpath, files), files])
 for f in file:
     fr = open(f[0],encoding='utf-8').read()
-    fd[f[1]] = fr
+    fd.append({
+        "content":fr,
+        "name":f[1]
+    })
 open('exported.js', 'w', encoding='utf-8').write("answerKey = " + json.dumps(fd, indent=4, sort_keys=True))
