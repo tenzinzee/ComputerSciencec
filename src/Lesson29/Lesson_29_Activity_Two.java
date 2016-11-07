ed19cc3caf209ed14236886431764f71cd7694a0
walterhuang <huangyuehao008@hotmail.com>
2016-11-07 09:39:17 -0500

    Add Lesson29

---
+++ src/Lesson29/Lesson_29_Activity_Two.java
@@ -0,0 +1,46 @@
+/*
+ * Lesson 29 Coding Activity 2
+ * Write a loop that processes an array of strings. 
+ * Each String should be printed backwards on its own line.
+ * 
+ * For example, if the list contains:
+ *     
+ *     {"every", "nearing", "checking", "food", "stand", "value"}
+ * 
+ * It should output:
+ *     yreve
+ *     gniraen
+ *     gnikcehc
+ *     doof
+ *     dnats
+ *     eulav
+ */ 
+
+import java.util.Scanner;
+import java.lang.Math; 
+
+class Lesson_29_Activity_Two {
+  
+    /* Fill this list with values that will be useful for you to test. 
+   * A good idea may be to copy/paste the list in the example above. 
+   * Do not make any changes to this list in your main method. You can 
+   * print values from list, but do not add or remove values to this 
+   * variable.  
+   */
+    public static String [] list = {"every", "nearing", "checking", "food", "stand", "value"};
+
+    public static String reverse(String a){
+        String result = "";
+        for (int i = a.length() - 1; i >= 0; i--){
+            result = result.concat(a.substring(i,i+1));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        for(int i = 0; i < list.length; i++) {
+            System.out.println(reverse(list[i]));
+        }
+    }
+
+}
